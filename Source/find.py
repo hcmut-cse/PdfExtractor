2527e689d5bed5b5eb45712ff87ea48f73bcc199
trkhoi <39881166+trkhoi@users.noreply.github.com>
2019-08-16 21:11:11 +0700

    New Keyword (new version)

--- Source/find.py
+++ Source/find.py
@@ -8,7 +8,8 @@ from preprocess import preProcessPdf
 from processData import extractData
 from removeHeaderFooter import removeHeaderAndFooter
 from removeWatermark import removeWatermark
-import re 
+import re
+import string
 
 CURR_KW = {}
 def similar(a, b):
@@ -33,7 +34,7 @@ def countKeyword(CONFIG):
                         if (count == len(CURR_KW)):
                             CURR_KW[key1] = 1
                             count = 0
-def splitWord(fullPdf, listCheck):
+def detectInData(fullPdf, listCheck):
     listPdf = []
     for line in fullPdf:
         line = line.lower()
@@ -41,6 +42,17 @@ def splitWord(fullPdf, listCheck):
         listLine = re.split("\\s \\s+", line)
         listPdf.append(listLine)
     #print(listPdf)  
+    # for listLine in listPdf:
+    #     for key in list(CURR_KW):
+    #         for ele in listLine:
+    #             ratio = similar(ele, key)
+    #             if (ratio >= 0.8):
+    #                 ele = ele.replace(":","")
+    #                 ele = ele.strip()
+    #                 if ele not in listCheck:
+    #                     listCheck.append(ele)
+    #                     note = ele + " is a new keyword"
+    #                     print(note)
     for listLine in listPdf:
         for key in list(CURR_KW):
             for ele in listLine:
@@ -49,23 +61,43 @@ def splitWord(fullPdf, listCheck):
                     ele = ele.replace(":","")
                     ele = ele.strip()
                     if ele not in listCheck:
+                        listCheck.append(ele)
                         note = ele + " is a new keyword"
                         print(note)
-
+def detectNotInData(fullPdf, listCheck):
+    listPdf = []
+    for line in fullPdf:
+        line = line.lower()
+        listLine = []
+        listLine = re.split("\\s \\s+", line)
+        listPdf.append(listLine)
+    for listLine in listPdf:
+        for ele in listLine:
+            result = re.findall(r'[\s\w\\/\\."]+[\s]*:', ele)
+            for i in result:
+                if ":" in i:
+                    x = re.search(":", i)
+                    keyword = i[0:x.start()]
+                    keyword = keyword.strip()
+                    #print(keyword)
+                    if keyword not in listCheck:
+                        keyword = keyword + " is a new keyword"
+                        print(keyword)
 if __name__ == '__main__':
-    # Take keyword from others template to make data
-    PDF = ["1","2","3","5","6","7","8","9","10","12","17"]
+    PDF = ["1","2","3","5","6","7","8","9","10","11","12","16","17"]
+
+    # take keyword from folder 4 to check
     with open('../' + "4" + '/' + "4" + '.json', 'r', encoding='utf8') as json_file:
         ORIGINAL_CONFIG_CHECK = json.load(json_file)
     CONFIG_CHECK = ORIGINAL_CONFIG_CHECK[0].copy()
-    # take keyword from folder 4 to check
     listCheck = []
     for key in CONFIG_CHECK:
         key = key.lower()
         key = re.sub(r'[0-9]+', '', key)
         key = key.replace("_","")
         listCheck.append(key)
- 
+
+    # Take keyword from others template to make data
     for PDF_TYPE in PDF:
         fileName = list(filter(lambda pdf: pdf[-3:] == 'pdf' ,os.listdir('../' + PDF_TYPE)))
         with open('../' + PDF_TYPE + '/' + PDF_TYPE + '.json', 'r', encoding='utf8') as json_file:
@@ -87,13 +119,19 @@ if __name__ == '__main__':
                 CURR_CONFIG[key]['row'] = CONFIG[key]['row'].copy()
                 CURR_CONFIG[key]['column'] = CONFIG[key]['column'].copy()
             countKeyword(CURR_CONFIG)
-    #print("Keyword in data:")
+
     print("----------------------------------------------------------------")
-    # for kw in CURR_KW:
-    #     print(kw)
+    print("Keyword in data:")
+    for kw in CURR_KW:
+        print(kw)
+    print("----------------------------------------------------------------")
+    print("Keyword in CONFIG 4:")
+    for key in listCheck:
+        print(key)
+    print("----------------------------------------------------------------")
     PDF_TYPETEST = "4"
     file = "SGNV56782400_1_SI.pdf"
-    print("----------------------------------------------------------------")
     print(file)
     fullPdf, removed = preProcessPdf('../' + PDF_TYPETEST + '/' + file, HF_CONFIG)
-    splitWord(fullPdf, listCheck)
+    detectInData(fullPdf, listCheck)
+    detectNotInData(fullPdf, listCheck)
