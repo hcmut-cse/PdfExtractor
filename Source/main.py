8e5f4908e10986727cc6b4de0570b5c32363e763
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2019-08-25 21:02:00 +0700

    Update main.py

--- Source/main.py
+++ Source/main.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import json
 import numpy as np
-from preprocess import preProcessPdf
+from preProcess import preProcessPdf
 from processData import extractData
 from posProcess import posProcessData
 from connectContent import connectContent
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     PDF_TYPE = str(args.pdf_type)
 
     fileName = list(filter(lambda pdf: pdf[-3:].lower() == 'pdf' ,os.listdir('../' + PDF_TYPE)))
-    # fileName = ["CLTV01102313_SGNV00095400_1_ocps_si@opussmtp.one-line.com_1(ocps20190110110324_1)_1.pdf"]
+    # fileName = ["16.pdf"]
 
     for file in fileName:
         with open('../' + PDF_TYPE + '/' + PDF_TYPE + '.json', 'r', encoding='utf8') as json_file:
